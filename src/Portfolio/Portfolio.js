15e74542c4676c8a518f00335aecdd9057195737
Davin Henrik <73198042+davinTheDeveloper@users.noreply.github.com>
2021-08-20 09:29:33 -0400

    4th update

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -28,40 +28,34 @@ const Portfolio = () => {
             </div>
             <div className="fit">
                 <div className="container1">
-                <h3 className="title1">Non-Profit Template</h3>
-                <ul className="list1">
-                    <li>React, CSS, HTML5</li>
+                <h2 className="title1">Non-Profit Template</h2>
+                    <li className="try">React, CSS, HTML5</li>
                     <li>Landing page w/ working hamburger</li>
                     <li>Easy navigation / Caring 1st impression</li>
-                </ul>
                 <div className="imgContainer1">
             <Link className="image1" to="https://davinthedeveloper.github.io/nonprofitTemplate/" target="_blank" rel="noreferrer">
             </Link>
             </div>
             </div>
             <div className="container2">
-            <h3 className="title2">Pomodoro Timer</h3>
-            <ul className="list2">
+            <h2 className="title2">Pomodoro Timer</h2>
                 <li>React, CSS, HTML5</li>
-                <li>Functioning Play/Pause/Stop Buttons</li>
-                <li>Ability to adjust Study/Break durations</li>
-                <li>Displays time current remaining</li>
+                <li>Play/Pause/Stop Buttons</li>
+                <li>Adjustable Time</li>
+                <li>Counter display</li>
                 <li>Graphical Bar representing time passed</li>
-            </ul>
             <div className="imgContainer2">
             <Link className="image2" to="https://davinthedeveloper.github.io/pomodoro/" target="_blank" rel="noreferrer">
             </Link>
             </div>
             </div>
             <div className="container3">
-            <h3 className="title3">Matching Characters</h3>
-            <ul className="list3">
+            <h2 className="title3">Matching Characters</h2>
                 <li>Javascript, CSS, HTML5</li>
-                <li>Kids Game to become comfortable with the keyboard</li>
-                <li>Click button to generate a random letter and number</li>
-                <li>Input the same characters into their respective box</li>
-                <li>Checks for correct letter case and matching characters</li>
-            </ul>
+                <li>Children's game (3-5yrs)</li>
+                <li>Generates a random letter and number</li>
+                <li>Find and input those characters</li>
+                <li>Code checks for matches and letter case</li>
             <div className="imgContainer3">
             <Link className="image3" to="https://davinthedeveloper.github.io/compareCharacters.github.io/" target="_blank" rel="noreferrer">
             </Link>
