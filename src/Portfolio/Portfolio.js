89e173fecc310893359f021a022b57479a7c34fa
Davin Henrik <73198042+davinTheDeveloper@users.noreply.github.com>
2021-08-20 07:39:26 -0400

    Update Portfolio.js

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Portfolio.css';
 
-function Portfolio() {
+const Portfolio = () => {
     return (
         <div className="backColor">
             <div className="Header">
