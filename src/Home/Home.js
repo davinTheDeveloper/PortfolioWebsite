0f90d2dc47ccb84e6c67350fb8bc2cba439c0358
Davin Henrik <73198042+davinTheDeveloper@users.noreply.github.com>
2021-08-25 11:47:53 -0400

    Aug 25

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Home.css';
-import davin from '../davin.jpeg';
 
 const Home = () => {
         return (
             <div className="homeContainer">
                 <h1 className="name">Davin Henrik</h1>
                 <div className="davin">
-                    <img src={davin} alt="self"/>
                     </div>
             </div>
         );
